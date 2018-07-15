59b2f58f9fc7cb414d1ec6b1b4f1ddb6f50f66e1
IsidroGH <isidrogarciahernandez@gmail.com>
2018-07-15 23:31:24 +0200

    improvements

--- src/main/java/com/pickzen/tinzen/controller/TinzenController.java
+++ src/main/java/com/pickzen/tinzen/controller/TinzenController.java
@@ -2,35 +2,40 @@ package com.pickzen.tinzen.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.pickzen.api.shared.controller.BaseController;
+import com.pickzen.tinzen.controller.beans.NewSessionBean;
 import com.pickzen.tinzen.service.TinzenService;
 import static com.pickzen.api.shared.Utils.stringToList;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="tinzen")
-public class TinzenController extends BaseController {
+public class TinzenController {
+	private static final Logger log = LoggerFactory.getLogger(TinzenController.class);
 	
 	@Autowired
 	private TinzenService tinzenService;	
 	
 	@RequestMapping(value="/new_session", method=RequestMethod.POST)
-	public Map<String, Object> newSession() {
+	public ResponseEntity<NewSessionBean> newSession() {
+		log.debug("Tinzen: new_session");
 		String sessionId = UUID.randomUUID().toString();
 		//tinzenService.setUser(sessionId);
 		
-		Map<String,Object> resp = new HashMap<>();
-		resp.put("id",sessionId);
-		
-		return resp;
+		return new ResponseEntity<>(new NewSessionBean(sessionId), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value="/new_item/{iid}", method=RequestMethod.POST)
